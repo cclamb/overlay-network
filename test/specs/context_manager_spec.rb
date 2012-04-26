49e8f701a40d6ac2f5474453e946b6f68bab9285
cclamb <cclamb@cs.unm.edu>
2012-04-26 05:15:12 -0600

    happy path context manager working

--- test/specs/context_manager_spec.rb
+++ test/specs/context_manager_spec.rb
@@ -4,6 +4,28 @@ require 'json'
 
 require_relative '../../lib/koi/context_manager'
 
+def parse_response response
+  JSON.parse(last_response.body)['level'].to_sym
+end
+
+def set_and_test new_level
+    post '/status', {:level => new_level}
+    last_response.should be_ok
+    get '/status'
+    last_response.should be_ok
+    parse_response(response).should eq new_level
+end
+
+def set_and_test_unknown new_level
+    get '/status'
+    last_response.should be_ok
+    post '/status', {:level => new_level}
+    last_response.should be_ok
+    get '/status'
+    last_response.should be_ok
+    parse_response(response).should eq new_level
+end
+
 describe Koi::ContextManager do
   include Rack::Test::Methods
 
@@ -19,12 +41,13 @@ describe Koi::ContextManager do
   end
 
   it 'should handle post /status' do
-    post '/status', {:level => :secret}
-    last_response.should be_ok
-    get '/status'
-    last_response.should be_ok
-    status = JSON.parse last_response.body
-    status['level'].should eq 'secret'
+    set_and_test :secret
+    set_and_test :unclassified
+    set_and_test :top_secret
+  end
+
+  it 'should reject unknown settings' do
+
   end
 
 end
