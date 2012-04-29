2e40a81ad3d7513f161f010b3aa36361f8b1a555
cclamb <cclamb@cs.unm.edu>
2012-04-29 14:22:26 -0600

    rectification in place and specified

---
+++ lib/util/content_rectifier.rb
@@ -0,0 +1,70 @@
+require 'nokogiri'
+
+module Util
+
+  class ContentRectifier
+
+    @@Ordering = [:unclassified, :secret, :top_secret]
+
+    def rectify license, artifact, level = :unclassified
+
+      return nil if license == nil || artifact == nil
+
+      license_doc = Nokogiri::XML license
+      artifact_doc = Nokogiri::XML artifact
+
+      sensitives = get_sensitives license_doc
+      sensitives.each do |sensitive|
+
+        restrictions = sensitive.search 'restriction'
+        restrictions.each do |restriction|
+
+          if restriction[:property] == 'transmit'
+            #puts "R: #{restriction[:function]} #{restriction.content}\n"
+
+            ok_to_xmit = evaluate restriction[:function], \
+            level, restriction.content.strip
+
+            if !ok_to_xmit
+              #puts "//*/#{sensitive[:name]}"
+              artifact_doc.search("//*/#{sensitive[:name]}").each do |node|
+                #puts node.to_s
+                node.remove
+              end
+            end
+
+          end
+
+        end
+
+      end
+      artifact_doc.to_s
+    end
+
+    def get_sensitives doc
+      doc.xpath '//*/entity'
+    end
+
+    def evaluate relation, content_level, link_level
+      #puts "#{relation} : #{link_level} : #{content_level}"
+      content_level = content_level.to_sym
+      link_level = link_level.to_sym
+      case relation.to_sym
+        when :less_than
+          ordering(content_level) < ordering(link_level)
+        when :greater_than
+          ordering(content_level) > ordering(link_level)
+        when :equal
+          ordering(content_level) == ordering(link_level)
+        else raise 'unknown level type'
+      end
+    end
+
+    def ordering level
+       @@Ordering.index level
+    end
+
+
+  end
+
+end
---
+++ test/specs/content_rectifier_spec.rb
@@ -0,0 +1,50 @@
+require_relative '../../lib/util/content_rectifier'
+
+ARTIFACT_FILENAME = 'etc/sample_artifact.xml'
+LICENSE_FILENAME = 'etc/sample_license.xml'
+
+def read_and_rectify rectify_level, op_size, s_size
+  cr = Util::ContentRectifier.new
+  artifact_xml = File.read ARTIFACT_FILENAME
+  license_xml = File.read LICENSE_FILENAME
+  xml = cr.rectify license_xml, artifact_xml, rectify_level
+  doc = Nokogiri::XML xml
+  operationals = doc.search '//artifact/test/operational'
+  sources = doc.search '//artifact/test/source'
+  operationals.size.should eq op_size
+  sources.size.should eq s_size
+end
+
+describe Util::ContentRectifier do
+
+  it 'should be creatable' do
+    Util::ContentRectifier.new.should_not eq nil
+  end
+
+  it 'should handle nil submissions by returning nil' do
+    cr = Util::ContentRectifier.new
+    cr.rectify(nil, nil).should eq nil
+    cr.rectify("", nil).should eq nil
+    cr.rectify(nil, "").should eq nil
+  end
+
+  it 'should raise an error if non-string are submitted' do
+    cr = Util::ContentRectifier.new
+    ->() { cr.rectify 1,"" }.should raise_error
+    ->() { cr.rectify "", 1 }.should raise_error
+    ->() { cr.rectify 2, 1 }.should raise_error
+  end
+
+  it 'should not filter content at TS' do
+    read_and_rectify :top_secret, 1, 1
+  end
+
+  it 'should filter some content at S' do
+    read_and_rectify :secret, 1, 0
+  end
+
+  it 'should filter additional content at UC' do
+    read_and_rectify :unclassified, 0, 0
+  end
+
+end
